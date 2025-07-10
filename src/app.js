d094e321e056be95396bf549a7707c5330400162
Daniel Castro <100711317+DanCasV27@users.noreply.github.com>
2025-07-10 00:07:04 -0600

    adding routes for authentication in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
+const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
+app.use('/api/auth',authRoutes); 
 
 app.use(errorHandler);
 
