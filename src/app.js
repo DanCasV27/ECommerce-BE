f551a994f222946883e90d519488b88c640c945a
Daniel Castro <100711317+DanCasV27@users.noreply.github.com>
2025-07-18 00:17:40 -0600

    changes for cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/products', productRoutes);
 app.use('/api/auth',authRoutes); 
